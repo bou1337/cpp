93782da80da79218683e3adf8ffcb48c4f4dbf85
bou1337 <aitbougnsa.ismail@gmail.com>
2025-10-15 16:18:23 +0100

    cpp07

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,69 +1,80 @@
-
-#ifndef  ARRAY_H 
+#ifndef ARRAY_H
 #define ARRAY_H
-template <typename  T> 
 
-class Array 
-{   private :
-    T *data ; 
-    unsigned int size ;
-    public :
-   Array(): data(nullptr),size(0)
-   {
-        std::cout<<"Default constructor called\n"  ; 
-   }
-   Array(unsigned  int n):data(nullptr) , size(n)
-   {    if(n>0)
-        data  =  new T[n]();
-        std:: cout<<"constructor called\n" ;
+#include <iostream>
+#include <stdexcept>
 
-   } 
-   Array(Array &obj): data(nullptr) , size(obj.size) 
-   {   if(obj.size>0)
-        data  = new T[obj.size]() ; 
-        if(data)
+template <typename T>
+class Array
+{
+private:
+    T *data;
+    unsigned int size;
+
+public:
+    
+    Array() : data(nullptr), size(0)
+    {
+        std::cout << "Default constructor called\n";
+    }
+
+    Array(unsigned int n) : data(nullptr), size(n)
+    {
+        if (n > 0)
+            data = new T[n]; 
+        std::cout << "Constructor called\n";
+    }
+    Array(const Array &obj) : data(nullptr), size(obj.size)
+    {
+        if (obj.size > 0)
         {
-            for(unsigned i=0 ; i<size ;i++)
-            {
-                data[i] = obj.data[i] ;
-            }
-            size =  obj.size ;
+            data = new T[obj.size];
+            for (unsigned int i = 0; i < size; ++i)
+                data[i] = obj.data[i]; 
         }
-        std::cout<<"Copy constructor called\n" ; 
-    }  
-   Array& operator =(Array &obj)
-   {    if(obj!=*this)
-        {
-            delete[] data ;
-            size = obj.size ; 
-            if(size>0)
-            {
-                data = new T[size]() ;
-                
-                for(unsigned int i = 0 ; i<size ; i++)
-                {
-                    data[i]  = obj.data[i] ;
-                }
-            }
+        std::cout << "Copy constructor called\n";
+    }
 
+    Array &operator=(const Array &obj)
+    {
+        if (this != &obj) 
+        {
+            delete[] data;
+            size = obj.size;
+
+            if (size > 0)
+            {
+                data = new T[size];
+                for (unsigned int i = 0; i < size; ++i)
+                    data[i] = obj.data[i];
+            }
+            else
+            {
+                data = nullptr;
+            }
         }
-        std::cout<<"Assignment  operator called\n" ;
-        return *this ; 
-   } 
-   T  &operator[](unsigned int & index)
-   {   
-        if(index>=size)
-        throw "index is out of bound\n" ;
-        return data[index] ;
-   }
-   unsigned int size_fnc()
-   {
-    return  size ; 
-   }
-   ~Array()
-   {    
-        delete [] data ;
-        std::cout<<"Destructor called\n" ;
-   }
-} ;
+        std::cout << "Assignment operator called\n";
+        return *this;
+    }
+
+    T &operator[](unsigned int index)
+    {
+        if (index >= size)
+            throw std::out_of_range("Index out of bounds");
+        return data[index];
+    }
+    
+    unsigned int size_fnc() const
+    {
+        return size;
+    }
+
+    ~Array()
+    {
+        delete[] data;
+        data = nullptr;
+        std::cout << "Destructor called\n";
+    }
+};
+
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -5,21 +5,61 @@
 
 
 int  main()
-{   
+{
     try
     {
-
-        Array<int> a(10) ;
-        unsigned int  i = 0 ; 
-        while(i<a.size_fnc()+3)
+        Array<int> a ;
+        Array<int> b(20) ;
+        for(int  i = 0 ;i<20;i++)
         {
-            std::cout<<a[i]<<"\n";
-            i++ ; 
+            std::cout<<b[i]<<"," ;
         }
-    }
-    catch(const char *exp)
-    {
-        std:: cout<<exp<<"\n" ; 
-    }
+        std::cout<<"\n" ;
+        std::cout<<"*****************************************************************************************\n" ;
+        
+        a = b ; 
+        a[0] = 200 ;
+        for(int i = 0 ; i<20 ; i++)
+        {
+            std::cout<<a[i]<<"," ;
+        }
+        std:: cout<<"\n" ;
+        std::cout<<"*****************************************************************************************\n" ;
+        
 
+        Array<int> c(a) ;
+        for(int i = 0 ; i<20 ; i++)
+        {
+            std::cout<<c[i]<<"," ;
+        }
+        std::cout<<"\n" ;
+        std::cout<<"*****************************************************************************************\n" ;
+        
+
+    }
+    catch(const std::exception  &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        Array<char>  a(10) ;
+        for(int i = 0 ; i<10;i++)
+        {
+            a[i] = i+'a' ;
+        }
+        for(int i = 0 ; i<10 ; i++)
+        {
+            std::cout<<a[i]<<"," ;
+        } 
+         std:: cout<<"\n" ;               
+        std::cout<<"*****************************************************************************************\n" ;
+       
+        a[10] = 'k' ;
+    }
+    catch(const std::exception  &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 }
