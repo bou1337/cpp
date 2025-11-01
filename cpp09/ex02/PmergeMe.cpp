2a5a010359e47082302a464e9d92dc2fbb7fce0d
bou1337 <aitbougnsa.ismail@gmail.com>
2025-11-01 19:01:39 +0100

    cpp09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -50,3 +50,115 @@ double getTimeDiff(struct timespec &start, struct timespec &end)
     
     return diff_ns / 1000.0;
 }
+
+void PmergeMe(std::vector<int> &data)
+{
+    if (data.size() <= 1)
+        return;
+
+    typedef std::vector<int>::iterator Iterator;
+
+    int stray;
+    bool hasStray = false;
+    if (data.size() % 2 != 0)
+    {
+        stray = data.back();
+        data.pop_back();
+        hasStray = true;
+    }
+
+    std::vector<int> S;
+    
+    std::vector<int> pend; 
+    
+    for (Iterator it = data.begin(); it != data.end(); )
+    {
+        int val1 = *it++;
+        int val2 = *it++;
+
+        if (val1 > val2)
+        {
+            S.push_back(val1);
+            pend.push_back(val2);
+        }
+        else
+        {
+            S.push_back(val2);
+            pend.push_back(val1);
+        }
+    }
+
+    PmergeMe(S); 
+    for (size_t i = 0; i < pend.size(); ++i)
+    {
+        int& val_to_insert = pend[i];
+        
+        Iterator insert_pos = std::lower_bound(S.begin(), S.end(), val_to_insert);
+        
+        S.insert(insert_pos, val_to_insert);
+    }
+
+    if (hasStray)
+    {
+        Iterator insert_pos = std::lower_bound(S.begin(), S.end(), stray);
+        S.insert(insert_pos, stray);
+    }
+
+    data = S;
+}
+
+void PmergeMe(std::list<int> &data)
+{
+    if (data.size() <= 1)
+        return;
+
+    typedef std::list<int>::iterator Iterator;
+
+    int stray;
+    bool hasStray = false;
+    if (data.size() % 2 != 0)
+    {
+        stray = data.back();
+        data.pop_back();
+        hasStray = true;
+    }
+
+    std::list<int> S;
+    std::vector<int> pend; 
+    
+    for (Iterator it = data.begin(); it != data.end(); )
+    {
+        int val1 = *it++;
+        int val2 = *it++;
+
+        if (val1 > val2)
+        {
+            S.push_back(val1);
+            pend.push_back(val2);
+        }
+        else
+        {
+            S.push_back(val2);
+            pend.push_back(val1);
+        }
+    }
+
+    PmergeMe(S);
+
+    for (size_t i = 0; i < pend.size(); ++i)
+    {
+        int& val_to_insert = pend[i];
+        
+        Iterator insert_pos = std::lower_bound(S.begin(), S.end(), val_to_insert);
+        
+        S.insert(insert_pos, val_to_insert);
+    }
+
+    if (hasStray)
+    {
+        Iterator insert_pos = std::lower_bound(S.begin(), S.end(), stray);
+        S.insert(insert_pos, stray);
+    }
+
+    data = S;
+}
